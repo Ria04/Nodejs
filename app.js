aed97cfc89f577da5239e67f3d4c76e29419b741
RiYo <you@example.com>
2020-03-23 18:33:23 +0530

    Initing

--- app.js
+++ app.js
@@ -16,20 +16,20 @@ var port = process.env.PORT||8080;
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b9666b6c251254',
+    password: 'c10571db',
     database: 'vision'
 });
 
 // connect to database
-/*db.connect((err) => {
+db.connect((err) => {
     if (err) {
         throw err;
     }
     console.log('Connected to database');
 });
-global.db = db;*/
+global.db = db;
 
 // configure middleware
 app.set('port', process.env.PORT || port); // set express to use this port
